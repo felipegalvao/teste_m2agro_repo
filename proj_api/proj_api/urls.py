d0a772631a87854ddd1e7944f6ca3228289d53e2
Felipe Galvao <contato@felipegalvao.com.br>
2017-02-06 12:10:43 -0200

    Minor changes for URL organization

--- proj_api/proj_api/urls.py
+++ proj_api/proj_api/urls.py
@@ -22,15 +22,25 @@ from api_servicos import views as servicos_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^produtos/$', safra_produto_views.ProdutoList.as_view()),
-    url(r'^produtos/atualizar_preco_medio/$', safra_produto_views.ProdutoUpdatePrecoMedio),
-    url(r'^produtos/(?P<pk>[0-9]+)/$', safra_produto_views.ProdutoDetail.as_view()),
-    url(r'^safras/$', safra_produto_views.SafraList.as_view()),
-    url(r'^safras/(?P<pk>[0-9]+)/$', safra_produto_views.SafraDetail.as_view()),
-    url(r'^servicos/$', servicos_views.ServicoList.as_view()),
-    url(r'^servicos/(?P<pk>[0-9]+)/$', servicos_views.ServicoDetail.as_view()),
-    url(r'^linhas_servicos/$', servicos_views.LinhaServicoList.as_view()),
-    url(r'^linhas_servicos/(?P<pk>[0-9]+)/$', servicos_views.LinhaServicoDetail.as_view()),
+
+    # URLs da API de produtos e safra
+    url(r'^produtos/$', safra_produto_views.ProdutoList.as_view(), name='produtos_list'),
+    url(r'^produtos/atualizar_preco_medio/$', safra_produto_views.ProdutoUpdatePrecoMedio,
+        name='produtos_update_preco_medio'),
+    url(r'^produtos/(?P<pk>[0-9]+)/$', safra_produto_views.ProdutoDetail.as_view(),
+        name='produtos_detail'),
+    url(r'^safras/$', safra_produto_views.SafraList.as_view(), name='safra_list'),
+    url(r'^safras/(?P<pk>[0-9]+)/$', safra_produto_views.SafraDetail.as_view(),
+        name='safra_detail'),
+
+    # URLs da API de serviços
+    url(r'^servicos/$', servicos_views.ServicoList.as_view(), name='servicos_list'),
+    url(r'^servicos/(?P<pk>[0-9]+)/$', servicos_views.ServicoDetail.as_view(),
+        name='servicos_detail'),
+    url(r'^linhas_servicos/$', servicos_views.LinhaServicoList.as_view(),
+        name='linhas_servicos_list'),
+    url(r'^linhas_servicos/(?P<pk>[0-9]+)/$', servicos_views.LinhaServicoDetail.as_view(),
+        name='linhas_servicos_detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
